06843c950e424a1ba9212425bcad73923e69fd83
Corrado Santoro <santoro@dmi.unict.it>
2024-01-17 16:57:08 +0100

    moved

--- tests/test.cmake
+++ tests/test.cmake
@@ -1,8 +1,5 @@
 add_executable (test test.cpp)
 
-include_directories (../lib)
-link_directories (${CMAKE_SOURCE_DIR}/lib)
-
 target_link_libraries (test democle)
 if(THREADS_HAVE_PTHREAD_ARG)
   target_compile_options( test PUBLIC "-pthread")
